89f8f4020e95580198606390b9efc4902911f24b
Zufarbek <zufarbek.abdurakhmonov@gmail.com>
2023-06-13 00:44:08 +0500

    urls discommited

--- repositories/Repository.js
+++ repositories/Repository.js
@@ -5,8 +5,8 @@ const baseDomain = 'http://api.fibro.uz:3026'; // API for products
 // export const basePostUrl = 'http://localhost:1337'; // API for post
 // export const baseStoreURL = 'http://localhost:1337'; // API for vendor(store)
 
-// export const basePostUrl = process.env.BASE_POST_URL; // API for post
-// export const baseStoreURL = process.env.BASE_STORE_URL; // API for vendor(store)
+export const basePostUrl = process.env.BASE_POST_URL; // API for post
+export const baseStoreURL = process.env.BASE_STORE_URL; // API for vendor(store)
 
 export const customHeaders = {
     Accept: 'application/json',
